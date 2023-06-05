597d63602a0409ac50c4f7ddff9d61aeeb2afc2f
Xavier Mitault <xavier.mitault@epitech.eu>
2023-06-05 09:35:32 +0200

    Add print to test.c

--- test.c
+++ test.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+int print_something(int a)
+{
+    printf("%d ", a);
+}
+
 int main(void)
 {
     int a = 0;
 
     for (int i = 0; i < 10; i++) {
         a += i;
+        print_something(a);
     }
     return a;
 }
