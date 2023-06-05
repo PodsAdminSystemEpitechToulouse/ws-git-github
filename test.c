dc311b7b8d0ddee0bed961ccbfe0ad0ccb536ef1
Xavier Mitault <xavier.mitault@epitech.eu>
2023-06-05 09:37:39 +0200

    Print all in test.c

--- test.c
+++ test.c
@@ -3,6 +3,12 @@
 int print_something(int a)
 {
     printf("%d ", a);
+    return 0;
+}
+
+void flush_stdout(void)
+{
+    fflush(stdout);
 }
 
 int main(void)
@@ -12,6 +18,7 @@ int main(void)
     for (int i = 0; i < 10; i++) {
         a += i;
         print_something(a);
+        flush_stdout();
     }
     return a;
 }
